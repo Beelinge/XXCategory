d3e6d84c44b4cc83fdacb1dad091bb46be6af1d4
linzhipei <beelin9527@foxmail.com>
2017-07-10 20:24:33 +0800

    commit XXCategory

---
+++ XXCategory/UIImage/UIImage+Color.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+Color.h
+//  XXCategory
+//
+//  Created by Beelin on 17/6/23.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Color)
+
++ (UIImage *)xx_imageWithColor:(UIColor *)color;
++ (UIImage *)xx_imageWithColor:(UIColor *)color size:(CGSize)size;
+@end
