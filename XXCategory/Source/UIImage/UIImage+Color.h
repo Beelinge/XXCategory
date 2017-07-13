366fcd57c5433cecb7c1db2981ea8702fa9c6428
linzhipei <beelin9527@foxmail.com>
2017-07-13 17:57:50 +0800

    nothing

---
+++ XXCategory/Source/UIImage/UIImage+Color.h
@@ -0,0 +1,51 @@
+//
+//  UIImage+Color.h
+//  XXCategory
+//
+//  Created by Beelin on 17/6/23.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** 渐变方向 */
+typedef NS_ENUM(NSInteger, UIImageGradientDirection) {
+    UIImageGradientDirectionRight, //左-->右
+    UIImageGradientDirectionDown  //上-->下
+};
+
+@interface UIImage (Color)
+
+/**
+ *  获取图片
+ *
+ *  @param color 颜色
+ *
+ *  @return 颜色图片
+ */
++ (UIImage *)xx_imageWithColor:(UIColor *)color;
+
+/**
+ *  获取图片，，
+ *
+ *  @param color 颜色
+ *  @param size  宽高
+ 
+ *  @return 颜色图片
+ */
++ (UIImage *)xx_imageWithColor:(UIColor *)color
+                          size:(CGSize)size;
+
+/**
+ *   获取渐变的图片
+ *
+ *  @param bounds bounds
+ *  @param colors 数组颜色
+ *  @param direction 方向
+ *
+ *  @return 渐变的图片
+ */
++ (UIImage *)xx_gradientImageWithBounds:(CGRect)bounds
+                              direction:(UIImageGradientDirection)direction
+                                 colors:(NSArray *)colors;
+@end
---
+++ XXCategory/Source/UIImage/UIImage+Color.m
@@ -0,0 +1,59 @@
+//
+//  UIImage+Color.m
+//  XXCategory
+//
+//  Created by Beelin on 17/6/23.
+//  Copyright © 2017年 xx. All rights reserved.
+//
+
+#import "UIImage+Color.h"
+
+@implementation UIImage (Color)
+
++ (UIImage *)xx_imageWithColor:(UIColor *)color {
+    return  [self xx_imageWithColor:color size:CGSizeMake(1.0f, 1.0f)];
+}
+
++ (UIImage *)xx_imageWithColor:(UIColor *)color size:(CGSize)size {
+    CGRect rect = CGRectMake(0.0f, 0.0f, size.width, size.height);
+    UIGraphicsBeginImageContext(size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
++ (UIImage *)xx_gradientImageWithBounds:(CGRect)bounds  direction:(UIImageGradientDirection)direction colors:(NSArray *)colors {
+    if (!colors.count) return nil;
+    NSMutableArray *crefs = @[].mutableCopy;
+    [colors enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        UIColor *c = obj;
+        CGColorRef cref = c.CGColor;
+        [crefs addObject:(__bridge id _Nonnull)(cref)];
+    }];
+    
+    CALayer * bgGradientLayer = [self xx_gradientBGLayerForBounds:bounds colors:crefs.copy direction:direction];
+    UIGraphicsBeginImageContext(bgGradientLayer.bounds.size);
+    [bgGradientLayer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage * bgAsImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return bgAsImage;
+}
+
++ (CALayer *)xx_gradientBGLayerForBounds:(CGRect)bounds colors:(NSArray *)colors direction:(UIImageGradientDirection)direction
+{
+    CAGradientLayer * gradientBG = [CAGradientLayer layer];
+    gradientBG.frame = bounds;
+    gradientBG.colors = colors;
+    if (direction == UIImageGradientDirectionRight) {
+        gradientBG.startPoint = CGPointMake(0.0, 0);
+        gradientBG.endPoint = CGPointMake(1, 0);
+    } else {
+        gradientBG.startPoint = CGPointMake(0.0, 0);
+        gradientBG.endPoint = CGPointMake(0.0, 1.0);
+    }
+    return gradientBG;
+}
+@end
